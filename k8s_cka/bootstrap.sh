24c0b564df138acfebce4e95495bddacd72366cc
Md. Sakib Ibne Farhad <sakib.ibne.farhad@gmail.com>
2023-05-17 12:03:17 +0600

    add new vagrant setup for CKA cerification

--- k8s_cka/bootstrap.sh
+++ k8s_cka/bootstrap.sh
@@ -6,6 +6,8 @@ sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
-# Install packages
-echo "[TASK 2] Install packages"
-yum install -y htop zip unzip screen vim rsync
+# Update System
+sudo apt update
+sudo apt purge --autoremove snapd nano -y
+sudo apt install -y vim tmux jq htop curl
+sudo apt upgrade -y --autoremove
